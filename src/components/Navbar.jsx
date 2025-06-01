e43399ce178a54a3330f7a593015ffb97d1ff431
Omeed Malek <malekomeed@gmail.com>
2025-06-01 10:37:12 -0500

    Fix New Project button navigation link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = () => {
             </Link>
 
             <Link
-              to="/new-project/questionnaire"
+              to="/new-project"
               className="flex items-center gap-2 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
             >
               <Plus className="w-4 h-4" />
